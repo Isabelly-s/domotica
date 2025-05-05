423605e26a562f3311a67defdf85039d85e38f12
Isabelly-s <isabellydasilva@iesteis.net>
2025-05-05 09:34:10 +0200

    Create P5_Isabelly.py

---
+++ programas/P5_Isabelly.py
@@ -0,0 +1,35 @@
+Autor: Maria Isabelly Da Silva Cavalcante
+Data:05/05/2025
+
+from microbit import*
+import neopixel
+
+led=pin14
+np=neopixel.NeoPixel(pin13,1)
+pir=pin15
+
+while True:
+    if pir.read_digital()==1:
+        for i in range (0,1,1):
+            music.play(music.RINGTONE)
+        for i in range (0,4,1):
+            np=(255,0,0)
+            np.show()
+            sleep(500)
+            np=(0,0,0)
+            np.clear()
+            sleep(500)
+        for i in range (0,4,1):
+            led.write_digital(1)
+            sleep(500)
+            led.write _digital(0)
+            sleep(500)
+
+        display.show(Image.ANGRY)
+        SLEEP(5000)
+        display.clear()
+
+
+    else:
+        music.stop()
+    sleep(100)
